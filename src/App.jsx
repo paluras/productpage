7754ae05ee4d938f000b87d833df9fa7670aed3d
Palura Andrei <98059798+paluras@users.noreply.github.com>
2023-03-02 13:59:33 +0200

    configure Img for web

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ function App() {
 
   return (
     <div className="main">
-      <img className='left-side-web' src='src\assets\image-product-desktop.jpg' alt='img-left-web'></img>
-      <img className='left-side-mobile' src='src\assets\image-product-mobile.jpg' alt='img-left-mobile'></img>
+      <img className='left-side-web' src='https://user-images.githubusercontent.com/98059798/222422314-13395bce-95bc-4f8b-8e99-bb14bf89e2a1.jpg' alt='img-left-web'></img>
+      <img className='left-side-mobile' src='https://user-images.githubusercontent.com/98059798/222422317-e90a441b-dd7c-4d49-b40c-184c185010ca.jpg' alt='img-left-mobile'></img>
       <div className='right-side'>
         <h3 className='text-pretitle'>PERFUME</h3>
         <h1 className='product-title'>Gabrielle Essence Eau De Parfum</h1>
@@ -17,7 +17,7 @@ function App() {
           <p className='price-old'>$169.99</p>
         </div>
         <button>
-          <img src='src\assets\icon-cart.svg'></img>
+          <img src='https://user-images.githubusercontent.com/98059798/222422313-7ce115cb-fbb4-48c1-85ed-5e839895bfbe.svg'></img>
           Add to Cart
         </button>
       </div>
